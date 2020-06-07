21083944b16ff76964f9d3ff529e08301cdd8427
Tyler Grinn <tyler@tygr.info>
2020-06-06 21:13:15 -0400

    made notification scheduler more accurate

--- server/notifications/index.ts
+++ server/notifications/index.ts
@@ -4,22 +4,21 @@ import removeFutureNotifications from './remove-future-notifications'
 
 export { removeFutureNotifications }
 
-export default async () => {
-  send()
-
-  const nextMinute = new Date()
-  nextMinute.setSeconds(60, 0)
-  const delay = nextMinute.getTime() - new Date().getTime()
-
-  setTimeout(() => {
-    send()
-    setInterval(send, 60 * 1000)
-  }, delay)
-}
-
 const send = async () => {
   const now = new Date()
   now.setSeconds(0, 0)
   await createNotifications(new Date(now.getTime()))
   await sendNotifications(new Date(now.getTime()))
 }
+
+const scheduler = () => {
+  send()
+
+  const nextMinute = new Date()
+  nextMinute.setSeconds(60, 0)
+  const delay = nextMinute.getTime() - new Date().getTime()
+
+  setTimeout(scheduler, delay)
+}
+
+export default scheduler
