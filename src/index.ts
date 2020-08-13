5a627046bd06c41f4233b51273ef9ef419bbcb56
Tyler Grinn <tylergrinn@gmail.com>
2020-08-13 00:07:05 -0400

    added mysql database

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import startNotifications from './notifications';
 
 initSequelize().then(() => {
   startNotifications();
-  const PORT = process.env.API_PORT;
+  const { PORT } = process.env;
   // eslint-disable-next-line no-console
   app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });
